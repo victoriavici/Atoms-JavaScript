3a96249d0fce4b565b976eaee66d7604657eac8a
Victoria Gáliková <vikigalikova4@gmail.com>
2023-07-20 18:35:22 +0200

    fix: divided into more files

---
+++ player.js
@@ -0,0 +1,17 @@
+var Player = {};
+
+Player.listen = function() {
+    document.body.addEventListener("click", Player.click);
+}
+
+Player.click = function(e) {
+    var position = Draw.getPosition(e.target);
+    if (!position) { return; }
+
+    var x = position[0];
+    var y = position[1];
+    if (Board[x][y] < 4) {
+        Board[x][y]++;
+    }
+    Draw.all();
+}
